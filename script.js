db215d85b3b7cde4d927f22a8ed091c525752db3
Muxmoon <muxman.niyazbek@gmail.com>
2025-03-12 15:08:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,32 @@ const themeButton = document.getElementById("theme-toggle");
 themeButton.addEventListener("click", () => {
     document.body.classList.toggle("light-mode");
 });
+const form = document.getElementById("contact-form");
+const formStatus = document.getElementById("form-status");
+
+if (form) {
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(form);
+        const data = Object.fromEntries(formData.entries());
+
+        formStatus.textContent = "Sending...";
+
+        const response = await fetch("https://script.google.com/macros/s/AKfycby0fT7f_ZWodKjwKas6VZbNnOvHIEOC-A23JLnhzir6EHS9aP-X2Bctuan08BaDNckP/exec", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "application/json" }
+        });
+
+        if (response.ok) {
+            formStatus.textContent = "✅ Message sent!";
+            form.reset();
+        } else {
+            formStatus.textContent = "❌ Error sending message!";
+        }
+    });
+}
+
+
+//
